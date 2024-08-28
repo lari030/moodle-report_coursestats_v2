5f7cd1767519c821b75d0d631ad8be955c40462b
Lucas Carvalho Ferreira <lucas.ferreira11@estudante.ufla.br>
2024-08-28 14:28:32 -0300

    Início CoursestatsV2

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Report main page
+ *
+ * @package    report
+ * @copyright  2019 Paulo Jr
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require(__DIR__.'/../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+admin_externalpage_setup('reportcoursestatsv2', '', null, '', array('pagelayout'=>'report'));
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('heading', 'report_coursestats_v2'));
+echo $OUTPUT->footer();
