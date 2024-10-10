4bd974f36deeba95695200c159ae6033e8058b23
Lucas Carvalho Ferreira <lucas.ferreira11@estudante.ufla.br>
2024-10-10 15:27:04 -0300

    Criação de arquivos para mostrar tipos de uso e eventos

--- version.php
+++ version.php
@@ -22,6 +22,6 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024090210;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2024090212;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;         // Requires this Moodle version (see https://docs.moodle.org/dev/Releases)
 $plugin->component = 'report_coursestats_v2';  // Full name of the plugin
