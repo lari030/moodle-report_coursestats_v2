c1b2b06928e6037b8b2911fbe3a13ac27e69da7f
Paulo Júnior <pauloa.junior@ufla.br>
2025-03-11 13:36:43 -0300

    Atualize arquivo de versão do plugin

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025022704;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2025031100;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;         // Requires this Moodle version (see https://docs.moodle.org/dev/Releases)
 $plugin->component = 'report_coursestats_v2';  // Full name of the plugin
